513d6d30c4c456a5ca2fd804cb4e7bb27635c253
Mai Anh Nam <namma@geniee.co.jp>
2017-11-14 16:50:05 +0700

    Update 100.js: close browser after task done, and let the script run non-stop

--- scripts/100.js
+++ scripts/100.js
@@ -10,23 +10,28 @@ events.EventEmitter.defaultMaxListeners = 0;
 (async function run() {
 
     try {
-        const promises = [];
 
-        const now = Date.now();
-        for (let i = 0; i < 50; ++i) {
-            promises.push(launcher.launch().then(({ browser, page }) => {
-                // return checkAutoRedirection(browser, page);
-                return checkPopup(browser, page);
-            }));
+        while (true) {
+            const promises = [];
+
+            const now = Date.now();
+            for (let i = 0; i < 10; ++i) {
+                promises.push(launcher.launch().then(async ({ browser, page }) => {
+                    // return checkAutoRedirection(browser, page);
+                    const result = await checkPopup(browser, page);
+                    await browser.close();
+                    return result;
+                }));
+            }
+
+            const results = await Promise.all(promises);
+            const then = Date.now();
+            const period = then - now;
+
+            console.log('> Passed count:', results.filter(result => result === true).length);
+            console.log('> Failed count:', results.filter(result => result === false).length);
+            console.log('> Period:', period / 1000, 'sec');
         }
-
-        const results = await Promise.all(promises);
-        const then = Date.now();
-        const period = then - now;
-
-        console.log('> Passed count:', results.filter(result => result === true).length);
-        console.log('> Failed count:', results.filter(result => result === false).length);
-        console.log('> Period:', period / 1000, 'sec');
 
     } catch (err) {
         console.log(err);
