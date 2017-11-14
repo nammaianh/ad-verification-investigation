138f952aa9a211390b66143dfbe3b9fe9277ed6c
Mai Anh Nam <namma@geniee.co.jp>
2017-11-14 17:13:30 +0700

    Update working process of 100.js & log its result to logs/100.log

--- scripts/100.js
+++ scripts/100.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const { promisify } = require('util');
+const fs = require('fs');
+const appendFile = promisify(fs.appendFile);
+const path = require('path');
 const events = require('events');
 const launcher = require('./common/launcher');
 const checkAutoRedirection = require('./common/checkAutoRedirection');
@@ -9,13 +13,12 @@ events.EventEmitter.defaultMaxListeners = 0;
 
 (async function run() {
 
-    try {
-
-        while (true) {
+    while (true) {
+        try {
             const promises = [];
 
             const now = Date.now();
-            for (let i = 0; i < 10; ++i) {
+            for (let i = 0; i < 5; ++i) {
                 promises.push(launcher.launch().then(async ({ browser, page }) => {
                     // return checkAutoRedirection(browser, page);
                     const result = await checkPopup(browser, page);
@@ -28,13 +31,26 @@ events.EventEmitter.defaultMaxListeners = 0;
             const then = Date.now();
             const period = then - now;
 
-            console.log('> Passed count:', results.filter(result => result === true).length);
-            console.log('> Failed count:', results.filter(result => result === false).length);
-            console.log('> Period:', period / 1000, 'sec');
-        }
+            const logMsg =
+                `---------- ${new Date()} ----------\n` +
+                `> Passed count: ${results.filter(result => result === true).length}\n` +
+                `> Failed count: ${results.filter(result => result === false).length}\n` +
+                `> Period: ${period / 1000} sec\n`;
 
-    } catch (err) {
-        console.log(err);
+            // Write log to console
+            console.log(logMsg);
+
+            // Write log to file
+            appendFile(path.join(__dirname, '../logs/100.log'), logMsg);
+
+        } catch (err) {
+            const separator = `---------- ${new Date()} ----------\n`;
+            console.log(separator);
+            console.log(err);
+            await appendFile(LOG_FILE, separator);
+            await appendFile(LOG_FILE, err);
+            await appendFile(LOG_FILE, '\n');
+        }
     }
 
     process.exit();
