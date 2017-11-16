d2b647d96236b4eedb758e2fa199b1077211281b
Mai Anh Nam <namma@geniee.co.jp>
2017-11-16 17:11:18 +0700

    Update 100.js

--- scripts/100.js
+++ scripts/100.js
@@ -1,21 +1,33 @@
 'use strict';
 
-const { promisify } = require('util');
-const fs = require('fs');
-const appendFile = promisify(fs.appendFile);
-const path = require('path');
-const events = require('events');
-const launcher = require('./common/launcher');
+const { promisify }        = require('util');
+const fs                   = require('fs');
+const appendFile           = promisify(fs.appendFile);
+const path                 = require('path');
+const events               = require('events');
+const launcher             = require('./common/launcher');
 const checkAutoRedirection = require('./common/checkAutoRedirection');
-const checkPopup = require('./common/checkPopup');
-
-events.EventEmitter.defaultMaxListeners = 0;
+const checkPopup           = require('./common/checkPopup');
+const checkPopUnder        = require('./common/checkPopUnder');
 
 const LOG_FILE = path.join(__dirname, '../logs/100.log');
 
-const availableFunctions = { checkAutoRedirection, checkPopup };
-// const functionToCall = 'checkAutoRedirection';
-const functionToCall = 'checkPopup';
+const availableFunctions = {
+    checkAutoRedirection,
+    checkPopup,
+    checkPopUnder
+};
+const functionToCall     = process.argv[2];
+
+if (functionToCall === undefined) {
+    throw new Error('Function to call is missing');
+}
+if (availableFunctions[functionToCall] === undefined || typeof availableFunctions[functionToCall] !== 'function') {
+    throw new Error(`Function "${functionToCall}" is not available`);
+}
+
+events.EventEmitter.defaultMaxListeners = 0;
+
 
 (async function run() {
 
@@ -56,6 +68,7 @@ const functionToCall = 'checkPopup';
     process.exit();
 
 })();
+
 
 async function logError(err) {
     const separator = `---------- ${new Date()} ----------\n`;
